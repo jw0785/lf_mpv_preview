eabe3a80e29438074a659d4d878743b001439f37
jw0785 <143842176+jw0785@users.noreply.github.com>
2024-10-13 04:14:42 -0400

    fix filename error

--- LfMpvPreviewer_loader.cmd
+++ LfMpvPreviewer_loader.cmd
@@ -4,6 +4,6 @@ REM Powershell 5/6
 REM powershell -NoProfile -File %~dp0\MpvPreviewer.ps1 %0 %1 %2 %3 %4 %5 %6 %7 %8
 
 REM Powershell 7+
-pwsh -NoProfile -File %~dp0\MpvPreviewer.ps1 %0 %1 %2 %3 %4 %5 %6 %7 %8
+pwsh -NoProfile -File %~dp0\LfMpvPreviewer.ps1 %0 %1 %2 %3 %4 %5 %6 %7 %8
 REM disable lf preview cache
 exit /b 1
